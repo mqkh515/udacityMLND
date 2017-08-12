65046014d10db6e62fda018f2037bb7045a33b56
mqkh515 <mqkh515@163.com>
2017-08-13 02:54:49 +0800

    more updates for first submission of smartcab project

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -201,7 +201,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=50, tolerance = 0.01)
+    sim.run(n_test=50, tolerance=0.01)
     # sim.run()
 
 if __name__ == '__main__':
