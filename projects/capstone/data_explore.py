2c4d03b4ca95091d7c669491ada8ce7f4856c95b
mqkh515 <mqkh515@163.com>
2017-09-12 23:39:11 +0800

    capstone check in from home 20170912

--- projects/capstone/data_explore.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def get_train_data():
-    prop_data = pd.read_csv('data/properties_2016.csv', header=0)
-    error_data = pd.read_csv('data/train_2016_v2.csv', header=0)
-    train_data = error_data.merge(prop_data, on='parcelid')
