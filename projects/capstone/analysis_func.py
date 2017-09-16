e103b1be89b78db29699f43b482e617ba99c934c
mqkh515 <mqkh515@163.com>
2017-09-16 10:47:00 +0800

    capstone data explore finished. 20170916

--- projects/capstone/analysis_func.py
+++ projects/capstone/analysis_func.py
@@ -23,14 +23,19 @@ categorical_groups_keep_list['airconditioningtypeid'] = ['1', '13']
 # pairs
 num_bathroom_assessor, num_bathroom_zillow
 area_living_finished_calc, area_living_type_12
+3 types of dollar values: {dollar_taxvalue_structure, dollar_taxvalue_total, dollar_taxvalue_land}
 
 # direct impute missing
 fireplacecnt
 fullbathcnt
 
 # potentially can be improved
-num_fireplace ({1, nan}, other)
-num_fullbath (1, 2, other)
-area_garage use group mean to impute
+num_fireplace: ({1, nan}, other)
+num_fullbath: (1, 2, other)
+area_garage: use group mean to impute
+unitcnt: group 2-4, mark higher as NaN
+numberofstories: gorup 2,3, mark 4 as nan, make categorical
+
+# row removal
 
 
--- projects/capstone/zillow_1.py
+++ projects/capstone/zillow_1.py
@@ -35,6 +35,15 @@ def mark_flag_col(col_name):
     train_data.loc[train_data.index[~train_data_marks_true], col_name] = 'FALSE'
 
 
+def mark_flag_col_tax_delinquency():
+    prop_data_marks_true = prop_data['taxdelinquencyflag'] == 'Y'
+    prop_data.loc[prop_data.index[prop_data_marks_true], 'taxdelinquencyflag'] = 'TRUE'
+    prop_data.loc[prop_data.index[~prop_data_marks_true], 'taxdelinquencyflag'] = 'FALSE'
+    train_data_marks_true = train_data['taxdelinquencyflag'] == 'Y'
+    train_data.loc[train_data.index[train_data_marks_true], 'taxdelinquencyflag'] = 'TRUE'
+    train_data.loc[train_data.index[~train_data_marks_true], 'taxdelinquencyflag'] = 'FALSE'
+
+
 def col_fill_na(data, col, fill):
     if fill == 'mode':
         data[col].fillna(data[col].mode().values[0], inplace=True)
@@ -67,6 +76,18 @@ def raw_census_info_split():
     raw_census_info_split_inner(train_data)
 
 
+def census_info_split():
+    # create new categorical columns 'raw_census', 'raw_block'
+    def census_info_split_inner(data):
+        data['census'] = data['censustractandblock'].apply(lambda x: x[4:10] if not x == 'nan' else np.nan)
+        data['block'] = data['censustractandblock'].apply(lambda x: x[10:] if not x == 'nan' else np.nan)
+
+    prop_data['censustractandblock'] = prop_data['censustractandblock'].apply(lambda x: str(int(x)) if not np.isnan(x) else 'nan')
+    train_data['censustractandblock'] = train_data['censustractandblock'].apply(lambda x: str(int(x)) if not np.isnan(x) else 'nan')
+    census_info_split_inner(prop_data)
+    census_info_split_inner(train_data)
+
+
 def nan_impute_fullbathcnt():
     """nan impute fullbathcnt from bathroomcnt(which is no missing)"""
 
@@ -76,7 +97,7 @@ def nan_impute_fullbathcnt():
         fill_val_raw = fill_val.copy()
         fill_val_raw_floor = fill_val_raw.apply(math.floor)
         int_idx = np.abs(fill_val_raw.values - fill_val_raw_floor.values) < 1e-12
-        fill_val[int_idx] = np.maximum(fill_val_raw[int_idx] - 1)
+        fill_val[int_idx] = np.maximum(fill_val_raw[int_idx] - 1, 0)
         fill_val[~int_idx] = fill_val_raw_floor[~int_idx]
         data.loc[null_idx, 'fullbathcnt'] = fill_val
         return data
@@ -225,9 +246,43 @@ def property_cleaning_base():
 
     # num_34_bathroom
 
-    #
+    # type_construction
+    clear_cat_col_group(prop_data, 'typeconstructiontypeid', ['2'])
+    cat_num_to_str('typeconstructiontypeid')
 
-def property_cleaning_kaggle():
+    # num_unit
+
+    # area_yard_patio
+
+    # area_yard_storage
+
+    # year_built
+
+    # num_story
+    cat_num_to_str('numberofstories')
+
+    # flag_fireplace
+    mark_flag_col('fireplaceflag')
+
+    # dollar_taxvalue_structure
+
+    # dollar_taxvalue_total
+    col_fill_na(prop_data, 'taxvaluedollarcnt', 'mean')
+
+    # dollar_taxvalue_land
+    col_fill_na(prop_data, 'landtaxvaluedollarcnt', 'mean')
+
+    # dollar_tax
+
+    # flag_tax_delinquency
+    mark_flag_col_tax_delinquency()
+
+    # year_tax_due
+
+    # census_block
+
+
+def property_engineering():
     """for real kaggle submission, considering:
        1, outlier detection.
        2, missing data imputation from prop data.
@@ -235,7 +290,7 @@ def property_cleaning_kaggle():
     pass
 
 
-def prop_imputation(col_name):
+def property_imputation(col_name):
     pass
 
 
